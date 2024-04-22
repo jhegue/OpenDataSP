d2f22fe57c11ac1d6cf591294f482ea7456c6ab4
Joao Heguedusch <joao.heguedusch@proton.me>
2024-04-22 08:44:10 -0300

    feat

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import requests
 import pandas as pd
 import plotly.express as px
 
 st.title('Hello World')
-dados = 'tb_alunos_estrangeiros.csv'
+
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+regiao = st.sidebar.selectbox('Região', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_anos = st.sidebar.checkbox('Dados de todo o período', value = True)
+
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+url = 'https://labdados.com/produtos'
+query_string = {'regiao':regiao.lower(), 'ano':ano}
+response = requests.get(url, params = query_string)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+st.write(dados)
